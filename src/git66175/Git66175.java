45634d4f063e1610afbc5028b275aaa79901f869
Admin <Admin@DESKTOP-34T8ESC>
2021-02-23 22:40:24 -0600

    Ejercicio de class, ejemplo en clase

---
+++ src/git66175/Git66175.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git66175;
+import java.util.Scanner;
+/**
+ *
+ * @author Admin
+ */
+public class Git66175 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String cadena;
+        System.out.println("Miguel Alejandro Rivas Gomez ");
+        cadena = sc.nextLine();
+        System.out.println(" Mi nombre es = " + cadena);
+    }
+    
+}
